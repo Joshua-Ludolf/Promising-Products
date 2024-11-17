b4793cbb2e9674463e588707111434d644572d8f
Jonathon Davis <Jonathonh.davis@gmail.com>
2024-11-17 01:30:06 -0600

    removed pinata

--- hackutd-2024/src/utils/config.ts
+++
@@ -1,8 +0,0 @@
-import { PinataSDK } from "pinata"
-
-export const pinata = new PinataSDK({
-    // @ts-ignore
-  pinataJwt: `${import.meta.env.VITE_PINATA_JWT}`,
-  // @ts-ignore
-  pinataGateway: `${import.meta.env.VITE_GATEWAY_URL}`
-})
