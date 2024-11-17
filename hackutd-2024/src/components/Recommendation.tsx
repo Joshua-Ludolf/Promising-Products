eb026ff960a6817cfc3e173423f08029ad80ba70
jspinerola <144980273+jspinerola@users.noreply.github.com>
2024-11-17 11:36:48 -0600

    yuh

--- hackutd-2024/src/components/Recommendation.tsx
+++ hackutd-2024/src/components/Recommendation.tsx
@@ -5,6 +5,7 @@ import thumbsUp from "../assets/thumbs-up.svg";
 import thumbsDown from "../assets/thumbs-down.svg";
 import supabase from "../../supabase";
 import jsondata from "../../data.json";
+import Groq from "groq-sdk";
 
 interface RecommendationProps {
   data: {
@@ -17,12 +18,44 @@ interface RecommendationProps {
 }
 
 function Recommendation({ data, session }: RecommendationProps) {
+  const groq = new Groq({ apiKey: import.meta.env.VITE_GROQ_API_KEY, dangerouslyAllowBrowser: true });
+  
+
   const [insights, setInsights] = useState([]);
   const [dislikedIndex, setDislikedIndex] = useState<number | null>(null);
   const [plan, setPlan] = useState(null);
   const [addOns, setAddOns] = useState(null);
+  const [insight, setInsight] = useState(null);
 
   useEffect(() => {
+    async function getInsights(userData) {
+      const chatCompletion = await getGroqChatCompletion();
+
+      setInsight(chatCompletion);
+
+      async function getGroqChatCompletion() {
+        return groq.chat.completions.create({
+          messages: [
+            // Set an optional system message. This sets the behavior of the
+            // assistant and can be used to provide specific instructions for
+            // how it should behave throughout the conversation.
+            {
+              role: "system",
+              content:
+                "you are an assistant who needs to convince the user to purchase what they ask. Limit response to one sentence.",
+            },
+            // Set a user message for the assistant to respond to.
+            {
+              role: "user",
+              content: `Why should i buy this? Here is my data: ${JSON.stringify(
+                userData
+              )}`,
+            },
+          ],
+          model: "llama3-8b-8192",
+        });
+      }
+    }
     let acc_id = data.acct_id;
 
     let userData = jsondata[acc_id];
@@ -30,14 +63,18 @@ function Recommendation({ data, session }: RecommendationProps) {
     let plan = userData.insights[0].data.recommended_plan;
 
     let addOns = userData.insights[0].data.recommended_add_ons;
-    setPlan(plan)
-    setAddOns(addOns)
+    setPlan(plan);
+    setAddOns(addOns);
 
-    
+    getInsights(userData);
+
+
+    ;
   }, []);
 
-    console.log("plan " + JSON.stringify(plan));
-    console.log("addon " + addOns);
+  console.log("plan " + JSON.stringify(plan));
+  console.log("addon " + addOns);
+  console.log(insight)
 
   async function handleDislike(indexToRemove: number) {
     // console.log(indexToRemove);
@@ -59,13 +96,10 @@ function Recommendation({ data, session }: RecommendationProps) {
 
   return (
     <>
-      {plan &&
-        
-          <div
-            className="card relative bg-lg p-8 rounded-xl flex flex-col w-[346px]"
-          >
-            {/* Feedback Message */}
-            {/* <div
+      {plan && (
+        <div className="card relative bg-lg p-8 rounded-xl flex flex-col w-full">
+          {/* Feedback Message */}
+          {/* <div
               className={`feedback absolute w-full bg-red left-0 top-0 h-full flex justify-center items-center place-content-center text-center rounded-xl pointer-events-none transition-opacity duration-300 ${
                 dislikedIndex === index ? "opacity-100" : "opacity-0"
               }`}
@@ -76,19 +110,20 @@ function Recommendation({ data, session }: RecommendationProps) {
               </p>
             </div> */}
 
-            {/* Card Content */}
-            <div className="flex mb-2">
-              <img className="w-6 mr-2" src={stars} />
-              <h2 className=" text-xl text-left font-mono uppercase tracking-wide">
-                We recommend upgrading to...
-              </h2>
-            </div>
-            <p>{plan.name}</p>
-            <div className="mt-8 justify-between-b flex w-full">
-              <a target="_blank" rel="noopener noreferrer">
-                <Button text={"Learn More"} onClick={() => {}} />
-              </a>
-              {/* <div className="flex justify-center mx-auto">
+          {/* Card Content */}
+          <div className="flex mb-2 w-full">
+            <img className="w-6 mr-2" src={stars} />
+            <h2 className=" text-xl text-left font-mono uppercase tracking-wide">
+              Consider these upgrades!
+            </h2>
+          </div>
+          {insight && <p>{insight.choices[0].message.content}</p>}
+          
+          <div className="mt-8 justify-between-b flex w-full">
+            <a target="_blank" rel="noopener noreferrer">
+              <Button text={"Learn More"} onClick={() => {}} />
+            </a>
+            {/* <div className="flex justify-center mx-auto">
                 <button className="text-white bg-red h-[44px] p-4 rounded-full mr-1 hover:bg-blue transition-all">
                   <img src={thumbsUp} />
                 </button>
@@ -99,9 +134,9 @@ function Recommendation({ data, session }: RecommendationProps) {
                   <img src={thumbsDown} />
                 </button>
               </div> */}
-            </div>
           </div>
-        }
+        </div>
+      )}
     </>
   );
 }
